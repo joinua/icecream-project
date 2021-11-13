6ff398988cb8eb1caa75e5fd60437e2a20921fe5
joinua <56656852+joinua@users.noreply.github.com>
2021-11-13 08:21:22 +0000

    Deploying to gh-pages from @ joinua/icecream-project@7d13a6f2beacf4ae97ad08e60519d89ee5b75a84 🚀

--- src.479ed1ea.js
+++ src.479ed1ea.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/icecream-project/src.4ac59a4a.js.map
+//# sourceMappingURL=/icecream-project/src.479ed1ea.js.map
