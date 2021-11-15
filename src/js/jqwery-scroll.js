6ed12fa639ee45054253b0f97c0608e42f888217
Drummberg <drummberg@gmail.com>
2021-11-15 17:25:51 +0200

    Update jqwery-scroll.js

--- src/js/jqwery-scroll.js
+++ src/js/jqwery-scroll.js
@@ -1,7 +1,17 @@
-function slowScroll(id) { 
-         var offset = 0;
-         $('html, body').animate({ 
-              scrollTop: $(id).offset().top - offset 
-         }, 1000);
-         return false; 
-     } 
+$(document).ready(function () {
+  function slowScroll(id) {
+    var offset = 0;
+    $('html, body').animate(
+      {
+        scrollTop: $(id).offset().top - offset,
+      },
+      1000,
+    );
+    return false;
+  }
+  $('header').on('click', function (e) {
+    e.preventDefault();
+    if (!$(e.target).hasClass('header-nav__link')) return;
+    slowScroll($(e.target).attr('href'));
+  });
+});
