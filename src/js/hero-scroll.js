2f14bcbcbf209b044758e7cca8399621bc652a4b
Oleksandr Roshkevych <joinua@gmail.com>
2021-11-18 21:04:29 +0200

    Update hero-scroll.js

--- src/js/hero-scroll.js
+++ src/js/hero-scroll.js
@@ -1,11 +1,30 @@
-$ (function() {
-   $("a.scroll").click(function() {
-      $("html, body").animate({
-         scrollTop: $($(this).attr("href")).offset().top + "px"
-      }, {
-         duration: 1000,
-         easing: "swing"
-      });
-      return false;
-   });
+$(function () {
+  $('a.scroll').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top + 'px',
+      },
+      {
+        duration: 1000,
+        easing: 'swing',
+      },
+    );
+    return false;
+  });
+});
+
+$(function () {
+  $('.mobile-menu__link').on('click', function () {
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top + 'px',
+      },
+      {
+        duration: 1000,
+        easing: 'swing',
+      },
+    );
+    $('.js-menu-container').toggleClass('is-open');
+    return false;
+  });
 });
