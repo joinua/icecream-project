02c6c5407019aa12055abe226cf046679418a199
joinua <56656852+joinua@users.noreply.github.com>
2021-11-18 17:06:52 +0000

    Deploying to gh-pages from @ joinua/icecream-project@0059fd82c20104385bc11d5b70c872a814ec2dcf 🚀

--- src.e3a5d015.js
+++ src.e3a5d015.js
@@ -16,7 +16,7 @@ var i;!function(e){"use strict";"function"==typeof i&&i.amd?i(["jquery"],e):"und
 },{"jquery":"juYr"}],"wzuc":[function(require,module,exports) {
 $(function(){$(".rewiev__slider").slick({infinite:!0,dots:!0,arrows:!0,autoplay:!0,speed:300,waitForAnimate:!1})});
 },{}],"RSqK":[function(require,module,exports) {
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden"),document.body.classList.toggle("modal-open")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
 },{}],"uBnJ":[function(require,module,exports) {
 $(document).ready(function(){$("header").on("click",function(e){var t;(e.preventDefault(),$(e.target).hasClass("header-nav__link"))&&(t=$(e.target).attr("href"),$("html, body").animate({scrollTop:$(t).offset().top-0},1e3))})});
 },{}],"B9kg":[function(require,module,exports) {
@@ -32,4 +32,4 @@ const e=document.querySelector(".header-button2"),t=document.querySelector(".sub
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/preloader.js"),require("./js/slick.min.js"),require("./js/slider.js"),require("./js/modal.js"),require("./js/jqwery-scroll.js"),require("./js/readmore.js"),require("./js/scroll-to-top.js"),require("./js/hero-scroll.js"),require("./js/modal-contacts.js"),require("./js/mobile-form.js");
 },{"./sass/main.scss":"clu1","./js/preloader.js":"BeZ8","./js/slick.min.js":"SeYZ","./js/slider.js":"wzuc","./js/modal.js":"RSqK","./js/jqwery-scroll.js":"uBnJ","./js/readmore.js":"B9kg","./js/scroll-to-top.js":"mh2G","./js/hero-scroll.js":"jERz","./js/modal-contacts.js":"xcVw","./js/mobile-form.js":"UakX"}]},{},["Focm"], null)
-//# sourceMappingURL=/icecream-project/src.89b17968.js.map
+//# sourceMappingURL=/icecream-project/src.e3a5d015.js.map
